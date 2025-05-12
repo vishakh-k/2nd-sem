f8d14b740390bb772aeed8e27eee9a17502837d3
vishakh k t <vishakhkt.2003@gmail.com>
2025-05-12 20:44:48 +0530

    first programme added

---
+++ NumberTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class NumberTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the number triangle: ");
+        int rows = scanner.nextInt();
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
